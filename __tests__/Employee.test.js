c86746ae95c03b14656e5e401c259ee552e02450
Mediogre <caseyarrington13@gmail.com>
2021-04-23 21:10:53 -0500

    Create Employee object with tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,4 +8,29 @@ test('creates an employee object', () => {
     expect(employee.name).toBe('Frank');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
+    expect(employee.role).toBe('Employee');
+})
+
+test('gets employees name', () => {
+    const employee = new Employee('Frank', 3, 'frank@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+})
+
+test("gets employee's id", () => {
+    const employee = new Employee('Frank', 3, 'frank@gmail.com');
+
+    expect(employee.getId()).toEqual(employee.id);
+})
+
+test("gets employee's email", () => {
+    const employee = new Employee('Frank', 3, 'frank@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+
+test("gets employee's role/occupation", () => {
+    const employee = new Employee('Frank', 3, 'frank@gmail.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role.toString()));
 })
